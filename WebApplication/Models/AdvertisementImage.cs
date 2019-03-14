984a3c3872c2b566a309c1ce2fdba2c79c93cd75
niemamzdania <45124836+niemamzdania@users.noreply.github.com>
2019-03-14 11:10:04 +0100

    Add new column into AdvertisementImage table

--- WebApplication/Models/AdvertisementImage.cs
+++ WebApplication/Models/AdvertisementImage.cs
@@ -9,5 +9,6 @@ namespace WebApplication.Models
         [Required] [Column(TypeName = "varchar(100)")] public string AdvertisementId { get; set; }
         [Required] [Column(TypeName = "nvarchar(max)")] public string Image { get; set; }
         [Required] [Column(TypeName = "varchar(100)")] public string Description { get; set; }
+        [Required] [Column(TypeName = "varchar(100)")] public string Name { get; set; }
     }
 }
