8ec54f38f1433edcffd51ba03d207bd79be46f7c
Rafał Nowociński <vito24222@gmail.com>
2019-03-16 18:49:50 +0100

    add new class DTO

---
+++ WebApplication/DTO/AdvertisementsDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication.DTO
+{
+    public class AdvertisementsDTO
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public decimal Price { get; set; }
+        public decimal Size { get; set; }
+        public string Category { get; set; }
+
+        public AdvertisementsDTO(string City, string Street, decimal Price, decimal Size, string Category)
+        {
+            this.City = City;
+            this.Street = Street;
+            this.Price = Price;
+            this.Size = Size;
+            this.Category = Category;
+        }
+    }
+}
