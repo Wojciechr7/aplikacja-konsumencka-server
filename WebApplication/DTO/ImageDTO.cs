2cc37c8057572f6b0a353a7741e730d73b0ff799
Rafał Nowociński <vito24222@gmail.com>
2019-03-25 16:30:08 +0100

    removing constructors

--- WebApplication/DTO/ImageDTO.cs
+++ WebApplication/DTO/ImageDTO.cs
@@ -5,12 +5,5 @@
         public string Image { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
-
-        public ImageDTO(string Image, string Description, string Name)
-        {
-            this.Image = Image;
-            this.Description = Description;
-            this.Name = Name;
-        }
     }
 }
