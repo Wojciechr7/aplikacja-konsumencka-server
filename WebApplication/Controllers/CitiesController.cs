795c93dda1e354f57d6fdebfcd6ed6f68810e85e
Rafał Nowociński <vito24222@gmail.com>
2019-03-25 19:08:46 +0100

    add a city return with a database

---
+++ WebApplication/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public CitiesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cities>>> GetCities()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+    }
+}
