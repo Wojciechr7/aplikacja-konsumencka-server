2871b5f8cda4385767239c6d04e4846bc7456459
Rafał Nowociński <vito24222@gmail.com>
2019-03-25 18:29:37 +0100

    add new table Cities

---
+++ WebApplication/Models/Cities.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication.Models
+{
+    public class Cities
+    {
+             [Key] [Column(TypeName = "int")] public int Id { get; set; }
+        [Required] [Column(TypeName = "nvarchar(40)")] public string Name { get; set; }
+        [Required] [Column(TypeName = "nvarchar(30)")] public string Voivodeship { get; set; }
+    }
+}
