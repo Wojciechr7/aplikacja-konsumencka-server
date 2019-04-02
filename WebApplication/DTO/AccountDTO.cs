c19d29900b2e77e83ff1ca38c4eb1aa7bcc16c80
Rafał Nowociński <vito24222@gmail.com>
2019-04-02 16:28:17 +0200

    add return user id and phone number

--- WebApplication/DTO/AccountDTO.cs
+++ WebApplication/DTO/AccountDTO.cs
@@ -1,10 +1,14 @@
-﻿namespace WebApplication.DTO
+﻿using System;
+
+namespace WebApplication.DTO
 {
     public class AccountDTO
     {
         public string Token { get; set; }
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
